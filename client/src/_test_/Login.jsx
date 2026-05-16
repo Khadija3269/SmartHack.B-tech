5be6eae0e2d2b97a6686ddbb6706238edb6fde4d
Khadija3269 <16S2144@utas.edu.om>
2026-05-16 23:44:12 +0400

    Refactor Login test file for clarity

--- client/src/_test_/Login.jsx
+++ client/src/_test_/Login.jsx
@@ -6,6 +6,7 @@ import { Provider } from "react-redux";
 import { store } from "../redux/store";
 import "@testing-library/jest-dom";
 
+
 global.fetch = vi.fn(() =>
   Promise.resolve({
     json: () =>
