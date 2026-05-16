8d4039a4d9b5d2ef134b26df9a79bfca0909a7ff
Renad Al-Hamadani <lenren2003@gmail.com>
2026-05-16 23:52:39 +0400

    Refactor OrganizerDashboard component structure

--- client/src/components/OrganizerDashboard.jsx
+++ client/src/components/OrganizerDashboard.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import { SiteHeader } from "@/components/SiteHeader";
 import { SidebarProfile } from "@/components/SidebarProfile";
 import { getHackathons, refreshHackathons, refreshSubmissions, refreshUsers, stats, submissionsForOrganizer } from "@/lib/store";
-
+//for organizer
 function OrganizerDashboard() {
   const [s, setS] = useState({ hackCount: 0, partCount: 0, subCount: 0, userCount: 0 });
   const [activeUsers, setActiveUsers] = useState(0);
