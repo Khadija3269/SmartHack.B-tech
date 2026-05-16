4e22283804620db4ab8b0b2a8fd3a73f7cf23caf
Renad Al-Hamadani <lenren2003@gmail.com>
2026-05-16 23:42:37 +0400

    Remove unnecessary comment in Confirmation.jsx

--- client/src/components/Confirmation.jsx
+++ client/src/components/Confirmation.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import winner from "@/assets/winner.png";
-
+//for confirmation
 function ConfirmationPage() {
   return (<main className="min-h-screen flex items-center justify-center bg-white px-4 relative">
       <Link
